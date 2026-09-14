b1722d3821cdd9628d44450ad7ea1f9842d3215b
Paolam1903 <sernapaola922@gmail.com>
2026-09-14 16:59:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if not RUTA_LIQ.exists() or not RUTA_METAS.exists():
 # =============================
 st.markdown("""
 <div style="background-color:#E30613;padding:15px;border-radius:10px">
-<h1 style="color:white;text-align:center">📊 Dashboard Comercial y Accesorios al 13 - CVS PLUS al 10 – y Encuestas pendiente</h1>
+<h1 style="color:white;text-align:center">📊 Dashboard Comercial y Accesorios al 13 - CVS PLUS al 12 – y Encuestas pendiente</h1>
 </div>
 """, unsafe_allow_html=True)
 
